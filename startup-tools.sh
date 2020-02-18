aaab512a297c38a0bafcfc8e743137896f15c0b3
TimUntersberger <32014449+TimUntersberger@users.noreply.github.com>
2020-02-18 01:11:54 +0100

    Update startup-tools.sh

--- startup-tools.sh
+++ startup-tools.sh
@@ -1,5 +1,5 @@
 homesick clone https://github.com/TimUntersberger/dotfiles &&
-sudo apt install zsh &&
+sudo apt install zsh tmux -y &&
 curl -L git.io/antigen > ~/.antigen.zsh &&
 chsh $(which zsh) &&
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
@@ -8,6 +8,5 @@ sudo mv nvim.appimage /usr/bin/nvim &&
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
 vim +PlugInstall +q +q &&
-sudo apt install tmux &&
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
