b8393210997278e49b86d4be02d86e4d00bef39a
TimUntersberger <32014449+TimUntersberger@users.noreply.github.com>
2020-02-19 11:31:15 +0100

    Update startup-full.sh

--- startup-full.sh
+++ startup-full.sh
@@ -1,5 +1,5 @@
 curl https://raw.githubusercontent.com/TimUntersberger/dotfiles/master/startup-tools.sh | bash &&
-sudo apt install openssh-server x2goserver x2goserver-xsession i3 suckless-tools xterm feh -y &&
+sudo apt install openssh-server x2goserver x2goserver-xsession i3 suckless-tools gnome-terminal feh -y &&
 ssh-keygen -f newuser &&
 cat ~/.ssh/newuser.pub >> .ssh/authorized_keys &&
 sudo service ssh start &&
