839ae4e40164991c16e87de3df19a4364b887dec
TimUntersberger <32014449+TimUntersberger@users.noreply.github.com>
2020-04-03 17:44:04 +0200

    Update startup-full.sh

--- startup-full.sh
+++ startup-full.sh
@@ -1,2 +1,4 @@
 curl https://raw.githubusercontent.com/TimUntersberger/dotfiles/master/startup-tools.sh | bash &&
-sudo apt install i3 suckless-tools gnome-terminal feh -y
+sudo apt install i3 suckless-tools feh rofi -y &&
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh &&
+sudo ln -s ~/.local/kitty.app/bin/kitty /usr/bin/kitty
