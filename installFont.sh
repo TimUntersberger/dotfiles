f60166f8812b445aadfef0d69e03b3c21ae72bde
Tim Untersberger <timuntersberger2@gmail.com>
2020-04-24 16:48:43 +0200

    Add font install script

---
+++ installFont.sh
@@ -0,0 +1,2 @@
+ wget https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Mono.ttf\?raw\=true -O SauceCodeProNerdFont.ttf &&
+   mv SauceCodeProNerdFont.ttf ~/.local/share/fonts/SauceCodeProNerdFont.ttf
