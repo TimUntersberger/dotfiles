ae2413b82b2ac07ea9d8029af83f91d03b2f7635
TimUntersberger <32014449+TimUntersberger@users.noreply.github.com>
2020-04-25 10:27:18 +0200

    Update startup-tools.sh

--- startup-tools.sh
+++ startup-tools.sh
@@ -1,18 +1,19 @@
 sudo apt install ruby zsh tmux docker.io docker-compose git -y &&
 sudo gem install homesick &&
-sudo homesick clone https://github.com/TimUntersberger/dotfiles &&
-sudo curl -L git.io/antigen > ~/.antigen.zsh &&
-sudo curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
+homesick clone https://github.com/TimUntersberger/dotfiles &&
+curl -L git.io/antigen > ~/.antigen.zsh &&
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
 chmod u+x nvim.appimage && 
 sudo mv nvim.appimage /usr/bin/nvim &&
-sudo curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
 homesick link &&
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n &&
 chmod +x ./n &&
 sudo mv ./n /usr/bin/n &&
 sudo n lts &&
-nvim +PlugInstall +q +q &&
+nvim +PlugInstall +q +q! &&
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb &&
-sudo dpkg -i ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb &&
+rm ripgrep_11.0.2_amd64.deb
