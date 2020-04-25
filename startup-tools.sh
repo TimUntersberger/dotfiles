92d698183a41dcd63ef9824c02ae57a23192f1ed
TimUntersberger <32014449+TimUntersberger@users.noreply.github.com>
2020-04-25 10:19:33 +0200

    Update startup-tools.sh

--- startup-tools.sh
+++ startup-tools.sh
@@ -7,12 +7,12 @@ chmod u+x nvim.appimage &&
 sudo mv nvim.appimage /usr/bin/nvim &&
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
+homesick link &&
 curl -L https://raw.githubusercontent.com/tj/n/master/bin/n -o n &&
-sudo chmod +x ./n &&
-sudo mv ./n /usr/bin/n
+chmod +x ./n &&
+sudo mv ./n /usr/bin/n &&
 sudo n lts &&
 nvim +PlugInstall +q +q &&
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &&
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb &&
-sudo dpkg -i ripgrep_11.0.2_amd64.deb &&
-homesick link
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
